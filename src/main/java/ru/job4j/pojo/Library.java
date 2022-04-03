45348713c6d69b0697bc2f871ff0a92e59d1d8b2
MaksimZelenskii <zelmax1988@gmail.com>
2022-04-03 17:05:30 +0400

    Fixed Library class

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -4,15 +4,7 @@ import java.util.Objects;
 
 public class Library {
     public static void main(String[] args) {
-        Book brom = new Book("Krampus", 600);
-        Book pelevin = new Book("Generation V", 800);
-        Book geiman = new Book("American God's", 1000);
-        Book cleanCode = new Book("Clean code", 100);
-        Book[] books = new Book[4];
-        books[0] = brom;
-        books[1] = pelevin;
-        books[2] = geiman;
-        books[3] = cleanCode;
+        Book[] books = createBooks();
         for (Book b : books) {
             System.out.println(b.getTitle() + " - " + b.getNumberOfPages());
         }
@@ -29,5 +21,18 @@ public class Library {
                 System.out.println("Shown only books with title Clean code - " + b.getTitle() + " - " + b.getNumberOfPages());
             }
         }
+    }
+
+    private static Book[] createBooks() {
+        Book brom = new Book("Krampus", 600);
+        Book pelevin = new Book("Generation V", 800);
+        Book geiman = new Book("American God's", 1000);
+        Book cleanCode = new Book("Clean code", 100);
+        Book[] books = new Book[4];
+        books[0] = brom;
+        books[1] = pelevin;
+        books[2] = geiman;
+        books[3] = cleanCode;
+        return books;
     }
 }
