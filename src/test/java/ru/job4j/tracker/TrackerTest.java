36c5675c7779052715cd5fc61da6589044e9d50f
MaksimZelenskii <zelmax1988@gmail.com>
2022-04-05 18:54:14 +0400

    Fixed cheskstyle

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-
 public class TrackerTest {
     @Test
     public void whenTestFindById() {
