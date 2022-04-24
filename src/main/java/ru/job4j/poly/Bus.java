8a14ed622f0baddaced76e12f73d2341059e83b1
MaksimZelenskii <zelmax1988@gmail.com>
2022-04-24 22:00:57 +0400

    Added Bus methods implementations

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,16 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("Here goes!");
     }
 
     @Override
     public void passengers(int number) {
-
+        System.out.println(number + " passengers on board.");
     }
 
     @Override
     public double refuel(int amount) {
-        return 0;
+        return 5.57 * amount;
     }
 }
