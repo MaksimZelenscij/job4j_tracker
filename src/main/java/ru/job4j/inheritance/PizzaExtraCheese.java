3cb7568fb77a54e78a7c4b5f60b79ae276279c6f
MaksimZelenskii <zelmax1988@gmail.com>
2022-05-12 15:10:39 +0400

    Added  Pizza classes with overridden method

---
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
@@ -0,0 +1,14 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheese extends Pizza {
+    public PizzaExtraCheese() {
+        super();
+    }
+
+    @Override
+    public String name() {
+        return super.name() + " + Cheese";
+    }
+}
+
+
