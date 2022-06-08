02fa3be9a1ffba710c9b41162dce6668c1235740
MaksimZelenskii <zelmax1988@gmail.com>
2022-06-08 19:03:30 +0400

    Fixed PriorityQueue class

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -3,15 +3,15 @@ package ru.job4j.search;
 import java.util.LinkedList;
 
 public class PriorityQueue {
-    private LinkedList<Task> tasks = new LinkedList<>();
+    private final LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() > element.getPriority()) {
-                index++;
+            if (task.getPriority() < element.getPriority()) {
+                break;
             }
-            break;
+            index++;
         }
         this.tasks.add(index, task);
     }
