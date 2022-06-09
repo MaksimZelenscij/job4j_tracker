a987f7758296c24850c9aa9f37ae92ab03cfd5ee
MaksimZelenskii <zelmax1988@gmail.com>
2022-06-10 01:22:18 +0400

    Added PassportOffice class and test for it

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void duplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen duplicate = new Citizen("2f44a", "Maxim Zelensky");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(duplicate);
+        assertFalse(office.get(duplicate.getPassport()).getUsername().equals(duplicate.getUsername()));
+    }
+}
