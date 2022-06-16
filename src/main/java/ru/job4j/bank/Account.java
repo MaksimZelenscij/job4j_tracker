70ed148b2942a6ed78520d12c530ad57fd1a4026
MaksimZelenskii <zelmax1988@gmail.com>
2022-06-16 23:41:49 +0400

    Added JavaDoc class comments, fields in those classes

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,7 +2,17 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель данных счета
+ *
+ * @author Maxim Zelensky
+ */
+
 public class Account {
+    /**
+     * Хранение номера счета осуществляется в поле requisite
+     * Хранение остатка по счету осуществляется в поле balance
+     */
     private String requisite;
     private double balance;
 
@@ -11,21 +21,50 @@ public class Account {
         this.balance = balance;
     }
 
+    /**
+     * Метод позволяет получить номер счета
+     *
+     * @return возвращает номер счета
+     */
+
     public String getRequisite() {
         return requisite;
     }
+
+    /**
+     * Метод позволяет присвоить номер счета при его создании
+     *
+     * @param requisite номер счета и null, если номера нет
+     */
 
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод позволяет получить остаток по счету.
+     *
+     * @return остаток по счету или null, если остатка нет
+     */
+
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод позволяет назначить остаток по счету
+     *
+     * @param balance остаток по счету
+     */
+
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
+    /**
+     * Метод принимает на вход Object o, проверяет, что он совпадает с текущим объектом
+     * по полю requisite
+     */
 
     @Override
     public boolean equals(Object o) {
@@ -38,6 +77,12 @@ public class Account {
         Account account = (Account) o;
         return Objects.equals(requisite, account.requisite);
     }
+
+    /**
+     * Метод возращает hash - код объекта, сгенерированному по полю requisite
+     *
+     * @return возвращает hash - код объекта, сгенерированному по полю requisite
+     */
 
     @Override
     public int hashCode() {
