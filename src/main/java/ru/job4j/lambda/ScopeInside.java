b8968b297e6b9dee90df0a7087622998fc46a6a3
MaksimZelenskii <zelmax1988@gmail.com>
2022-07-01 00:28:46 +0400

    Added ScopeInside class

---
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+
+import java.util.function.Supplier;
+
+public class ScopeInside {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        int total1 = 0;
+        for (int number : numbers) {
+            int total = total1;
+            total1 = add(
+                    () -> total + number
+            );
+            System.out.println(total1);
+        }
+    }
+
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
+}
